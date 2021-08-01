445cb6d443718929ddc3f8c71804a201c8469683
okrzyzos <okrzyzos@outlook.fr>
2021-08-01 18:39:04 +0200

    olive

--- src/components/CheckBoxes.js
+++ src/components/CheckBoxes.js
@@ -2,60 +2,74 @@ import React, { Component } from 'react'
 import '../App.css';
 
 
-export default class CheckBoxes extends Component {
+
+export default class CheckBoxes extends Component  {
     state = {
-        allChecked: false,
-        list: [
-            {id:1, name: "item1", isChecked: false},
-            {id:2, name: "item2", isChecked: false},
-            {id:3, name: "item3", isChecked: false},
-            {id:4, name: "item4", isChecked: false},
-        ],
+      allChecked: false,
+      list: [
+        { id: 1, name: "item1", isChecked: false },
+        { id: 2, name: "item2", isChecked: false },
+        { id: 3, name: "item3", isChecked: false }
+      ]
     };
-
-
-handleChange = (e) => {
-let list =  this.state.list;
-let allChecked = this.state.allChecked;
-if(e.target.value === "checkAll"){
-    list.forEach(item => {
-        item.isChecked = e.target.checked;
-        allChecked = e.target.checked;
+// function which allows me to select all the checkboxes
+  handleChange = e => {
+    let itemName = e.target.name;
+    let checked = e.target.checked;
+    this.setState(prevState => {
+      let { list, allChecked } = prevState;
+      if (itemName === "checkAll") {
+        allChecked = checked;
+        list = list.map(item => ({ ...item, isChecked: checked }));
+      } else {
+        list = list.map(item =>
+          item.name === itemName ? { ...item, isChecked: checked } : item 
+        );
+        // When I click on all the checkboxes items the checkboxes select all is automatically checked
+        allChecked = list.every(item => item.isChecked);
+      }
+      return { list, allChecked };
     });
-}
-else{
-    list.find( item => item.name === e.target.name).isChecked = e.target.checked;
-}
-this.setState({list:list, allChecked: allChecked});
-}
+  };
 
-renderList = () => {
-
-
-return this.state.list.map(item =>
-    <div className="input-item">
-    
-        <input key={item.id} type="checkbox" name={item.name} value={item.name} checked={item.isChecked} onChange={this.handleChange} />
+  renderList = () => {
+    // I get my checkboxes items with the map function
+    return this.state.list.map(item => (
+      <div className="input-item" key={item.id}>
+        <input
+          type="checkbox"
+          name={item.name}
+          value={item.name}
+          checked={item.isChecked}
+          onChange={this.handleChange}
+        />
         <label>{item.name}</label>
+      </div>
+    ));
+  };
+  render() {
+    // return my checkboxes select all
+    return (
+      <div>
+      <div className="select">
+        <input
+          type="checkbox"
+          name="checkAll"
+          checked={this.state.allChecked}
+          onChange={this.handleChange}
+        />
 
-    </div>
-)
-}
-
-render(){
-return(
-    <>
-    <div className="select">
- <input  type="checkbox" 
-        value="checkAll" 
-        checked={this.state.allChecked} 
-        onChange={this.handleChange} />
-        <label>Select All</label>
+        Select all 
+        <br />
         </div>
+        {/* return my checkboxes items */}
         {this.renderList()}
-    <br/>
+      </div>
+    );
+  }
+}
 
-    </>
-);
-}
-}
+
+
+
+
